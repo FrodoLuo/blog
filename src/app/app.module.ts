4591b9b7f550544ba806d0e355795f97d591df51
FrodoLuo <frodoluo@outlook.com>
2019-11-22 00:55:34 +0800

    feat: split modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,43 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IndexLayoutComponent } from './layout/index-layout/index-layout.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { MaterialModule } from './modules/material/material.module';
-import { IndexComponent } from './pages/index/index.component';
-import { MotionBackgroundComponent } from './components/motion-background/motion-background.component';
 import { HttpClientModule } from '@angular/common/http';
-import { BlogComponent } from './pages/blog/blog.component';
-import { DelayedPanelComponent } from './components/delayed-panel/delayed-panel.component';
-import { OnScrollShowDirective } from './directives/on-scroll-show.directive';
-import { ArticleComponent } from './pages/article/article.component';
-import { MarkdownModule } from 'ngx-markdown';
-import { ArticlePageResolver } from './router-resolvers/article-page.resolver';
-import { ArticleListResolver } from './router-resolvers/article-list.resolver';
+
+import { BlogModule } from './modules/blog/blog.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexLayoutComponent,
     ContentLayoutComponent,
-    IndexComponent,
-    MotionBackgroundComponent,
-    BlogComponent,
-    DelayedPanelComponent,
-    OnScrollShowDirective,
-    ArticleComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
-    MaterialModule,
     HttpClientModule,
-    MarkdownModule.forRoot(),
-  ],
-  providers: [
-    ArticlePageResolver,
-    ArticleListResolver
+    MaterialModule,
+
+    HomeModule,
+    BlogModule
   ],
   bootstrap: [AppComponent]
 })
