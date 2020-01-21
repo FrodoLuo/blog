10f8b66219f7cdf178ee181a0d2b06e726e2aac2
spaceluo <spaceluo@tencent.com>
2020-01-21 15:13:47 +0800

    fix: remove interceptor

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,8 +4,6 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { UniversalInterceptor } from './interceptors/universal.interceptor';
 
 @NgModule({
   imports: [
@@ -15,11 +13,6 @@ import { UniversalInterceptor } from './interceptors/universal.interceptor';
   ],
   bootstrap: [AppComponent],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: UniversalInterceptor,
-      multi: true
-    }
   ]
 })
 export class AppServerModule {}
